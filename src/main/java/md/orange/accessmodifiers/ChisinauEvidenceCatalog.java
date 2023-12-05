289cfd492f3df947bf6b5e5dbfafb57b52f0bffc
lungu01 <vlada.01.lungu@gmail.com>
2023-12-05 17:53:20 +0200

    Added the details about poopulation evidence by using various access modifiers

---
+++ src/main/java/md/orange/accessmodifiers/ChisinauEvidenceCatalog.java
@@ -0,0 +1,21 @@
+package md.orange.accessmodifiers;
+
+import md.orange.accessmodifiers.peopleevidence.Person;
+
+public class ChisinauEvidenceCatalog {
+    public static void main(String[] args) {
+
+        Person Vlada = new Person();
+        Vlada.setAge(20);
+
+
+
+
+
+    }
+
+
+
+
+
+}
